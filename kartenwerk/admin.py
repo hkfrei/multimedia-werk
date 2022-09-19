cbb774a980f6287b7e3f950cbb1379140bd36043
Hanskaspar Frei <hkfrei@karten-werk.ch>
2022-09-19 11:48:17 +0200

    feat: add blogposts to admin interface.

--- kartenwerk/admin.py
+++ kartenwerk/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Angebot, Referenz, Card, Preisplan, Preisplanfeature, Testimonial, Stakeholder
+from .models import Angebot, Referenz, Card, Preisplan, Preisplanfeature, Testimonial, Stakeholder, Blogpost
 
 # Register your models here.
 admin.site.register([Angebot, Referenz, Card,
-                    Preisplan, Preisplanfeature, Testimonial, Stakeholder])
+                    Preisplan, Preisplanfeature, Testimonial, Stakeholder, Blogpost])
