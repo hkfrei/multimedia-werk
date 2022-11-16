b89ea6f3961b4fb02181610a9d96960884d21540
Hanskaspar Frei <hkfrei@karten-werk.ch>
2022-11-16 09:23:40 +0100

    feat: add urls for a contact form and email success message.

--- kartenwerk/urls.py
+++ kartenwerk/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path("angebot/<int:pk>", views.AngebotDetail.as_view(), name="angebot_detail"),
     path("referenzen", views.ReferenzenView.as_view(), name="referenzen"),
     path("ueber-uns", views.AboutView.as_view(), name="about"),
-    path("blog", views.Blogposts.as_view(), name="blog")
+    path("blog", views.Blogposts.as_view(), name="blog"),
+    path("angebot/<int:angebot_id>/preisplan/<int:preisplan_id>/contact",
+         views.Contact.as_view(), name="preisplan_message"),
+    path("angebot/<int:angebot_id>/preisplan/<int:preisplan_id>/message/<int:message_id>",
+         views.SuccessMessage.as_view(), name="success_message")
 ]
