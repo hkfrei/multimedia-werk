b0f8c3a405a599e7068a9dfa105d08ebdc05a2af
Hanskaspar Frei <hkfrei@karten-werk.ch>
2022-11-16 09:23:01 +0100

    feat: add a form for a email message.

---
+++ kartenwerk/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm, TextInput, Textarea, EmailInput
+from .models import Message
+
+
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        fields = ['name', 'vorname', 'email', 'nachricht']
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control'}),
+            'vormanme': TextInput(attrs={'class': 'form-control'}),
+            'email': EmailInput(attrs={'class': 'form-control'}),
+            'nachricht': Textarea(attrs={'class': 'form-control', 'rows': 5})
+        }
