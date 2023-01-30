49b57daf13bb7f6549ddba98b6446cdbd3f78672
Hanskaspar Frei <hkfrei@karten-werk.ch>
2023-01-31 00:53:47 +0100

    feat: allow some important hosts.

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['karten-werk-website-rnlvbjxo5a-oa.a.run.app',
+                 'localhost', 'karten-werk.ch']
 
 
 # Application definition
