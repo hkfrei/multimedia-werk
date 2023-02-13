a9fa6c2c8dd3c55c66977480a6eda5f277b2dc7f
Hanskaspar Frei <hkfrei@karten-werk.ch>
2023-02-13 09:29:44 +0100

    feat: enable gzip compression on the uwsgi server.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 # exit when failure occurs
 set -e
-uwsgi --http "0.0.0.0:${PORT}" --module website.wsgi --master --processes 4 --threads 2 --buffer-size 32768 --mime-file /app/mime.types --mime-file /etc/mime.types
+uwsgi --http "0.0.0.0:${PORT}" --module website.wsgi --master --processes 4 --threads 2 --static-gzip-all --buffer-size 32768 --mime-file /app/mime.types --mime-file /etc/mime.types
