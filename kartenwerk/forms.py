cff48064221367cef21746d33dd093b45c2526c3
Hanskaspar Frei <hkfrei@karten-werk.ch>
2023-02-23 11:31:15 +0100

    feat: add a ContactMessageForm and rename the MessageForm to be more specific.

--- kartenwerk/forms.py
+++ kartenwerk/forms.py
@@ -1,10 +1,22 @@
 from django.forms import ModelForm, TextInput, Textarea, EmailInput
-from .models import AngebotMessage
+from .models import AngebotMessage, ContactMessage
 
 
-class MessageForm(ModelForm):
+class AngebotMessageForm(ModelForm):
     class Meta:
         model = AngebotMessage
+        fields = ['name', 'vorname', 'email', 'nachricht']
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control'}),
+            'vorname': TextInput(attrs={'class': 'form-control'}),
+            'email': EmailInput(attrs={'class': 'form-control'}),
+            'nachricht': Textarea(attrs={'class': 'form-control', 'rows': 5})
+        }
+
+
+class ContactMessageForm(ModelForm):
+    class Meta:
+        model = ContactMessage
         fields = ['name', 'vorname', 'email', 'nachricht']
         widgets = {
             'name': TextInput(attrs={'class': 'form-control'}),
