729b36b9b59d18fd562005a62f6ccef4bbef78fe
Hanskaspar Frei <hkfrei@karten-werk.ch>
2023-02-23 10:41:20 +0100

    feat: add the ContactMessage Model to the admin.

--- kartenwerk/admin.py
+++ kartenwerk/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Angebot, Referenz, Card, Preisplan, Preisplanfeature, Testimonial, Stakeholder, Blogpost, AngebotMessage
+from .models import Angebot, Referenz, Card, Preisplan, Preisplanfeature, Testimonial, Stakeholder, Blogpost, AngebotMessage, ContactMessage
 
 # Register your models here.
 admin.site.register([Angebot, Referenz, Card,
-                    Preisplan, Preisplanfeature, Testimonial, Stakeholder, Blogpost, AngebotMessage])
+                    Preisplan, Preisplanfeature, Testimonial, Stakeholder, Blogpost, AngebotMessage, ContactMessage])
